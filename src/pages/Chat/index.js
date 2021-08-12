a19ecf9a7354373f9efe9674af8170b3bf0c1044
Kevin Shikuku <68957078+kevinsshikuku@users.noreply.github.com>
2021-08-12 19:29:07 +0300

    Update index.js

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -7,6 +7,7 @@ import Message from "./message";
 import "./chat.css";
 import Fab from "./Fab";
 import { Close} from "@material-ui/icons";
+import LinearProgress from '@material-ui/core/LinearProgress';
 import SendMessage from "./sendmsg";
 
 
@@ -20,7 +21,10 @@ function Index() {
    return(
      <>
       <Header/>
-      <div className="Cloading"><h1>Open chat room</h1></div>
+      <div className="Cloading">
+        <p>OPEN CHAT</p>
+        <LinearProgress className="progress"/>
+      </div>
      </>
    )}
 
@@ -28,7 +32,6 @@ function Index() {
     <>
     <Header/>
     { !show && <div onClick={() => setShow(true)}> <Fab/> </div>}
-
     {show &&
     <div className="nav">
         <p onClick={() => setShow(false)}> <Close/> </p>
