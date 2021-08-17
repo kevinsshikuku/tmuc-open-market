e2ef27abd46ed21cac79f100f18d81ec767c9831
Kevin Shikuku <68957078+kevinsshikuku@users.noreply.github.com>
2021-08-17 09:28:38 +0300

    Update posts.js

--- src/store/posts.js
+++ src/store/posts.js
@@ -1,1 +1,50 @@
+import React, {createContext, useReducer, useContext} from 'react'
+
+//Two context one holding state other holding dispatch
+//Auth state context
+const PostStateContext = createContext();
+
+//Dispatch state context
+const PostDispatchContext = createContext();
+
+
+
+
+//Message reducer
+const   PostReducer = (state, action) =>{
+   const { posts } = state
+   switch(action.type){
+       case "ADD_POST":
+        let newPosts = [
+          action.payload, ...posts
+        ]
+
+        return {
+         ...state,
+         posts:  newPosts
+        }
+
+
+      default:
+       throw new Error(`unknown action type ${action.type}`)
+   }
+}
+
+
+/** Provider that will export and use in the App.js */
+export const PostProvider = ({children}) => {
+  const [state, dispatch] = useReducer(PostReducer, { posts: []   })
+   return(
+    <PostDispatchContext.Provider value={dispatch}>
+        <PostStateContext.Provider value ={state}>
+                   {children}
+        </PostStateContext.Provider>
+    </PostDispatchContext.Provider>
+   )
+}
+
+
+//Export what is held inside usecontext
+export const usePostState = () => useContext(PostStateContext);
+export const usePostDispatch = () => useContext(PostDispatchContext);
 
